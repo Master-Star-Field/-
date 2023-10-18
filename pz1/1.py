f77390bf6a9bc80719d9e2752d5456c5f437769a
Alex <zeroalex2015@gmail.com>
2023-10-19 00:32:26 +0300

    commit_1_1_zad

--- pz1/1.py
+++ pz1/1.py
@@ -1,1 +1,15 @@
 #first test
+import cv2
+import numpy as np
+
+# задание 1
+img = cv2.imread('pears.png')
+res_img = cv2.resize(img, (486, 732), cv2.INTER_NEAREST)
+
+cv2.imshow('res', img)
+cv2.waitKey(0)
+
+print(img.shape)
+cv2.imwrite('C:\с диска д вся инфа\img.png', img)
+
+
